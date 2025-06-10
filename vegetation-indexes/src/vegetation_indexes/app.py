558e8d2e95fa772fce908b13f107396eb3023eec
Fabrice Brito <fabrice.brito@terradue.com>
2025-06-10 09:41:44 +0200

    adds pattern-5

--- vegetation-indexes/src/vegetation_indexes/app.py
+++ vegetation-indexes/src/vegetation_indexes/app.py
@@ -3,13 +3,13 @@ from vegetation_indexes.pattern_1 import pattern_1
 from vegetation_indexes.pattern_2 import pattern_2
 from vegetation_indexes.pattern_3 import pattern_3
 from vegetation_indexes.pattern_4 import pattern_4
-
+from vegetation_indexes.pattern_5 import pattern_5
 from vegetation_indexes.pattern_7 import pattern_7
 
 @click.group()
 def app_group():
     pass
 
-for pattern in [pattern_1, pattern_2, pattern_3, pattern_4, pattern_7]:
+for pattern in [pattern_1, pattern_2, pattern_3, pattern_4, pattern_5, pattern_7]:
     app_group.add_command(pattern)
 
